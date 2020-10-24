c7e6cec19404b09d35f4ad9857a41bc95e688e4b
harsh-not-haarsh <anand.2@iitj.ac.in>
2020-10-24 15:32:25 +0530

    compressing using basic hashmap

---
+++ main.py
@@ -0,0 +1,33 @@
+def getFile():
+    f = open("file.txt", "r")
+    s = f.read()
+    n = len(s)
+
+    return s,n
+
+def createHashMap(s):
+    store = {} 
+    for word in s.split():
+        try:
+            store[word] += 1
+        except:
+            store[word] = 1
+    return store
+
+def compress(store, n):
+    newLength = n
+
+    for word in store:
+        if len(word)*store[word] > 4:
+            newLength -= len(word)*store[word] - 4
+
+    print("Original Size of the file:", n, "bytes")
+    print("Compressed Size of the file:", newLength, "bytes")
+    print()
+    print("% Compression:", int(10000*(n-newLength)/n)/100, "%")
+
+if __name__ == "__main__":
+    s, n = getFile()
+    store = createHashMap(s)
+
+    compress(store, n)
